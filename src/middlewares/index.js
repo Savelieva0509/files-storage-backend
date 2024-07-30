509f44bf51caa89d2b0a257953fb96090cdf9953
Babenko Liudmyla <savelieva0509@gmail.com>
2024-07-30 16:56:51 +0300

    add main structure and logics

---
+++ src/middlewares/index.js
@@ -0,0 +1,9 @@
+const validateBody = require("./validateBody");
+const isValidId = require("./isValidId");
+const validateFileSize = require("./validateFileSize");
+
+module.exports = {
+  validateBody,
+  isValidId,
+  validateFileSize,
+};
