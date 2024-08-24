51b6c8b9923c3aba85ddaa867a6b957fce7bebd0
Babenko Liudmyla <savelieva0509@gmail.com>
2024-08-24 13:49:51 +0300

    fix multer

--- src/routes/api/fileRoutes.js
+++ src/routes/api/fileRoutes.js
@@ -9,9 +9,16 @@ const {
 const router = express.Router();
 const multer = require("multer");
 
-const upload = multer({
-  limits: { fileSize: 7 * 1024 * 1024 * 1024 },
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/tmp/my-uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
 });
+const upload = multer({ storage: storage });
 
 router.get("/", ctrl.listFiles);
 
